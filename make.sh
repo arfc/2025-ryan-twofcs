36bc275e995ed025f14d083a575046c065388ab0
Nathan Ryan <nathansean0@gmail.com>
2025-04-03 19:39:46 -0500

    update slide algorithms to tikz

--- make.sh
+++ make.sh
@@ -1,8 +1,8 @@
-echo "Would you like to make the presentation (p), abstract (a), or both (b)?"
-read -p "(p/a/b): " build
+echo "Would you like to make the slides (s), abstract (a), paper (p), or everything (e)?"
+read -p "(s/a/p/e): " build
 echo "Ok, I will build that right away!"
 
-if [ $build == p ]
+if [ $build == s ]
 then
     (cd presentation && make all-via-pdf)
 fi
@@ -12,8 +12,14 @@ then
     (cd abstract && make all-via-pdf)
 fi
 
-if [ $build == b ]
+if [ $build == p ]
+then
+    (cd paper/doc/ && make all)
+fi
+
+if [ $build == e ]
 then
     (cd presentation && make all-via-pdf)
     (cd abstract && make all-via-pdf)
+    (cd paper/doc/ && make all)
 fi
